d0560476f00a7dc54de8c4086e32b9283641b266
Steve Trotta <steve.n.trotta@protonmail.com>
2022-12-04 10:53:42 -0500

    Finished Day 4

--- day_3/main.py
+++ day_3/main.py
@@ -1,4 +1,72 @@
+class Bag:
+    def __init__(self, contents):
+        self.contents = contents
+        compartment_size = len(contents) // 2
+        compartment1 = Compartment(contents[0:compartment_size])
+        compartment2 = Compartment(contents[compartment_size:])
+        self.compartment1 = compartment1
+        self.compartment2 = compartment2
+
+    def get_compartment_match(self):
+        return self.compartment1.find_match(self.compartment2)
+
+    def find_bag_match(self, bag2, bag3):
+        all_common = []
+        for character in bag3.contents:
+            if (character in bag2.contents) and (character not in all_common):
+                all_common.append(character)
+        for character in bag3.contents:
+            if (character not in self.contents) and (character in all_common):
+                all_common.remove(character)
+        return all_common
+
+
+class Compartment:
+    def __init__(self, contents):
+        self.contents = contents
+        self.all_common = []
+
+    def find_match(self, other_compartment):
+        for character in other_compartment.contents:
+            if character in self.contents:
+                return character
+
+
+def calculate_priority(match):
+    a_ord = ord('a')
+    a_ord_upper = ord('A')
+    if match.isupper():
+        return (ord(match) - a_ord_upper) + 27
+    else:
+        return (ord(match) - a_ord) + 1
+
+
+def calculate_bag_sum():
+    total = 0
+    group_total = 0
+    grouping = []
+    with open('input_day_3.txt') as input_file:
+        for line in input_file:
+            line = line.strip()
+            bag = Bag(line)
+            grouping.append(bag)
+            compartment_match = bag.compartment1.find_match(bag.compartment2)
+            priority = calculate_priority(compartment_match)
+            print("Compartment Match {}, Priority: {}".format(compartment_match, priority))
+            total += priority
+            if len(grouping) == 3:
+                grouping.sort(key=lambda curr_bag: len(curr_bag.contents))
+                group_match = grouping[0].find_bag_match(grouping[1], grouping[2])
+                priority = calculate_priority(group_match[0])
+                print("Group Match {}, Priority: {}".format(group_match, priority))
+                group_total += priority
+                grouping.clear()
+    print("Total Priority: {}".format(total))
+    print("Group Priority: {}".format(group_total))
+    return total
+
+
 # Execution of Day 3 of Advent of Code
 if __name__ == '__main__':
     # Execute
-    pass
+    calculate_bag_sum()
---
+++ day_4/main.py
@@ -0,0 +1,44 @@
+class Assignment:
+    def __init__(self, assignment_str):
+        start, end = assignment_str.split('-')
+        self.start = int(start)
+        self.end = int(end)
+
+    def contains(self, other_ass):
+        beginning_contained = other_ass.start >= self.start
+        end_contained = other_ass.end <= self.end
+        return beginning_contained and end_contained
+
+    def overlaps(self, other_ass):
+        other_ass_start_contained = self.start <= other_ass.start <= self.end
+        other_ass_end_contained = self.start <= other_ass.end <= self.end
+        return other_ass_start_contained or other_ass_end_contained
+
+
+def count_overlap():
+    contain_total = 0
+    overlap_total = 0
+    with open('input_day_4.txt') as input_file:
+        for line in input_file:
+            line = line.strip()
+            assignment_str1, assignment_str2 = line.split(',')
+            ass1 = Assignment(assignment_str1)
+            ass2 = Assignment(assignment_str2)
+            print(line)
+            if ass1.contains(ass2) or ass2.contains(ass1):
+                contain_total += 1
+                print("Yep")
+            else:
+                print("Nope")
+            if ass1.overlaps(ass2) or ass2.overlaps(ass1):
+                overlap_total += 1
+                print("Yep")
+            else:
+                print("Nope")
+    return contain_total, overlap_total
+
+
+# Execution of Day 3 of Advent of Code
+if __name__ == '__main__':
+    # Execute
+    print(count_overlap())
